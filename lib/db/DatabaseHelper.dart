fd014f229b221378e92452acf160aa0287709e40
Masyithah28 <masyithahsophia@gmail.com>
2024-10-17 23:07:49 +0700

    masyithah db

---
+++ lib/db/DatabaseHelper.dart
@@ -0,0 +1,48 @@
+2. DatabaseHelper.dart 
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:uts_sregep/models/ImageData.dart';
+
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper.();
+  static Database? _database;
+
+  DatabaseHelper._();
+
+  factory DatabaseHelper() => _instance;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'images.db');
+    return openDatabase(
+      path,
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE images(id INTEGER PRIMARY KEY AUTOINCREMENT, imagePath TEXT, usia TEXT, jenisKulit TEXT, kandungan TEXT)',
+        );
+      },
+      version: 1,
+    );
+  }
+
+  // Insert image data
+  Future<void> insertImageData(ImageData imageData) async {
+    final db = await database;
+    await db.insert('images', imageData.toMap());
+  }
+
+  // Retrieve all images
+  Future<List<ImageData>> getAllImages() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('images');
+    return List.generate(maps.length, (i) {
+      return ImageData.fromMap(maps[i]);
+    });
+  }
+}
